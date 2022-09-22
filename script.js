1dce5f35a317ad60330c00b6227af2977c7cc744
akanimoh <oakanimoh@gmail.com>
2022-09-22 14:34:25 +0100

    Updated files

--- script.js
+++ script.js
@@ -18,16 +18,16 @@ function createUserCard(user) {
     
     const card = `
         <div class = "card">
-            <div>
+            <div class = "img-container">
                 <img class = "avatar" src = "${user.avatar_url}" alt = "${user.name}"/>
             </div>
-            <div>
+            <div class = "user_info">
                 <h2>${user.name}</h2>
                 <p>${user.bio}</p>
                 <ul class = "info">
-                    <li>${user.public_repos}</li>
-                    <li>${user.twitter_username}</li>
-                    <li>${user.following}</li>
+                    <li><strong>Repos</strong>${user.public_repos}</li>
+                    <li><strong>Twitter</strong>${user.twitter_username}</li>
+                    <li><strong>Following </strong>${user.following}</li>
                 </ul>
             </div>
         </div>
